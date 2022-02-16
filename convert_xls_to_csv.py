ff3c1e290cd56d004be44d8206395f2e7d7fbfa6
danielwang546 <42781202+danielwang546@users.noreply.github.com>
2022-02-16 14:57:28 -0800

    Add data, start transferring files

---
+++ convert_xls_to_csv.py
@@ -0,0 +1,17 @@
+import os
+import pandas as pd
+
+src_folder = "xlsSamples"
+out_folder = "raw_data"
+
+count_states = {
+    "active": 16,
+    "meditate": 15,
+    "neutral": 15
+}
+
+for state in count_states:
+    for i in range (1, count_states[state] + 1):
+        read_file = pd.read_excel(os.path.join(src_folder, state + str(i) + ".xlsx"))
+        read_file.to_csv(os.path.join(out_folder, state + str(i) + ".csv"), index=False)
+
